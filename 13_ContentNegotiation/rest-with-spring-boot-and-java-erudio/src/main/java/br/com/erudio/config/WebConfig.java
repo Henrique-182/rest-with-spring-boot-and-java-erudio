2b7630884c70f732aa079a5dc7d2763dba6866ea
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-05-08 06:36:59 -0300

    Implementando Content Negotiation
    Implementando Content Negotiation via Header Parameter

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -10,9 +10,18 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		/*
 		configurer.favorParameter(true);
 		configurer.parameterName("mediaType");
 		configurer.ignoreAcceptHeader(true); 
+		configurer.useRegisteredExtensionsOnly(false);
+		configurer.defaultContentType(MediaType.APPLICATION_JSON);
+		configurer.mediaType("json", MediaType.APPLICATION_JSON);
+		configurer.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		
+		configurer.favorParameter(false);
+		configurer.ignoreAcceptHeader(false); 
 		configurer.useRegisteredExtensionsOnly(false);
 		configurer.defaultContentType(MediaType.APPLICATION_JSON);
 		configurer.mediaType("json", MediaType.APPLICATION_JSON);
