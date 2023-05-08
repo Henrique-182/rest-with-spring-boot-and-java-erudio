3e491a5bd78c32a6feff5ccb1d0491d4d0237ddf
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-05-08 06:23:10 -0300

    Implementando Content Negotiation
    Implementando Content Negotiation via Query Parameter

---
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,22 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(true);
+		configurer.parameterName("mediaType");
+		configurer.ignoreAcceptHeader(true); 
+		configurer.useRegisteredExtensionsOnly(false);
+		configurer.defaultContentType(MediaType.APPLICATION_JSON);
+		configurer.mediaType("json", MediaType.APPLICATION_JSON);
+		configurer.mediaType("xml", MediaType.APPLICATION_XML);
+	}
+
+}
--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -24,22 +24,22 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO findbyId(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 	
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
