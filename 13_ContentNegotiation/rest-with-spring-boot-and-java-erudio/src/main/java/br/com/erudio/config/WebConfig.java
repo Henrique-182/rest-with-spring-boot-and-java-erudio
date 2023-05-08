9eb25bc6dcf25e7d77b4ea9e7e50c9e4b88f0df0
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-05-08 07:35:21 -0300

    Serializando em YML

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,12 +1,24 @@
 package br.com.erudio.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.com.erudio.YamlJackson2HttpMessageConverter;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -26,6 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
 		configurer.defaultContentType(MediaType.APPLICATION_JSON);
 		configurer.mediaType("json", MediaType.APPLICATION_JSON);
 		configurer.mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
 	}
 
 }
