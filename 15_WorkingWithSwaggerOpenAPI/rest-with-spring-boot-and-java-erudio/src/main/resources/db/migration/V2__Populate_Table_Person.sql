4dbc05ace075175511d1af6858ec48594df4b909
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-06-06 06:16:11 -0300

    Seção 15

---
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (1,'Av Pedro Ludovico Teixeira','Henrique','Male','Augusto Lobo');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (2,'Av Pedro Ludovico Teixeira Qd50 Lt10 Cs2','Marina','Female','Lobo');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (3,'Av Pedro Ludovico Teixeira Qd50 Lt10 Cs2','Ricardo','Male','Augusto');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (4,'Av Vasco dos Reis','Samuel','Male','Sabunete');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (5,'São Paulo','Leandro','Male','Costa');
