74e7a81cdd9f1b79c835a1ff6d1ac12556a2c816
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-12-21 13:15:14 -0300

    Criando o Serviço Responsável por Consumir a API do ChatGPT

---
+++ 29_ChatgptApiWithSpringBoot/chatgpt-spring-boot-java-integration/src/main/java/br/com/erudio/vo/service/ChatGptService.java
@@ -0,0 +1,39 @@
+package br.com.erudio.vo.service;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.erudio.vo.request.ChatGptRequest;
+import br.com.erudio.vo.response.ChatGptResponse;
+
+@Service
+public class ChatGptService {
+
+	private Logger logger = Logger.getLogger(ChatGptService.class.getName());
+	
+	@Value("${openai.model}")
+	private String model;
+	
+	@Value("${openai.api.url}")
+	private String url;
+	
+	@Autowired
+	private RestTemplate template;
+	
+	public Object chat(String prompt) {
+		logger.info("Starting Chat");
+		
+		ChatGptRequest request = new ChatGptRequest(model, prompt);
+		
+		logger.info("Processing Prompt");
+		
+		ChatGptResponse response = template.postForObject(url, request, ChatGptResponse.class);
+		
+		return response;
+	}
+	
+}
